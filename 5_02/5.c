55e5c1882578f12ddab03b60995c7cccb0b79240
Sota20000121 <ntv-350-pvg@e-net.nara.jp>
2025-05-02 06:14:00 +0000

    5月2日

---
+++ 5_02/5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int score;
+
+    printf("点数を入力してください\n");
+    scanf("%d",&score);
+
+    if(score>=80){
+        printf("合格\n"); 
+    }else{
+        printf("不合格\n");
+    }
+
+    return 0;
+}
+
+
+
---
+++ 5_02/7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    int i;
+    scanf("%d",&i);
+    while(i>0){
+        printf("%d\n",i);
+        i--;
+    }
+
+return 0;
+}
